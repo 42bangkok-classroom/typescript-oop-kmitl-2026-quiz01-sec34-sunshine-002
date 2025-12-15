34ae9dea8fb224f5762fd547ffaee9071d1c4308
Sunshine <sunshine.17923@gmail.com>
2025-12-15 17:40:58 +0700

    aaa

---
+++ src/q13/light.ts
@@ -0,0 +1,15 @@
+let light = process.argv[2]?.toLowerCase()
+
+switch(light) {
+    case "red": 
+        console.log("Stop")
+        break
+    case "yellow":
+        console.log("Caution")
+        break
+    case "green":
+        console.log("Go")
+        break
+    default:
+        console.log("Unknown")
+}
